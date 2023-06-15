ad3df06bec6ad78ccb4b3bae208d733a3cb73767
dmahajan <Disha.Mahajan@allegion.com>
2023-06-15 12:33:40 -0700

    =added branch and basic home screen UI

--- app/src/main/java/com/sampleCompose/myapplication/MainActivity.kt
+++ app/src/main/java/com/sampleCompose/myapplication/MainActivity.kt
@@ -3,16 +3,25 @@ package com.sampleCompose.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sampleCompose.myapplication.ui.theme.SampleComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +31,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeUI()
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeUI(){
+    val cityState = remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleComposeAppTheme {
-        Greeting("Android")
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Input a City")
+
+        // Input field for the city
+        // Store the entered text in cityState.value
+        TextField(
+            value = cityState.value,
+            onValueChange = { cityState.value = it }
+        )
+
+        // Submit button
+        Button(onClick = { /* Handle submit logic here */ }) {
+            Text(text = "Submit")
+        }
     }
 }
